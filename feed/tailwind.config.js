5e24b358f878368006a33b9dc8c9edaa171f6a5d
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-08-29 16:03:11 -0500

    Added font and colors to tailwind.config

--- feed/tailwind.config.js
+++ feed/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#302F37", 
+        backgroundLight: "rgba(67, 66, 77, 0.3)",
+        text: "#E6FDFF", 
+        primary: "#0B5563", 
+        secondary: "#18BCDC",
+      },
+      fontFamily: {
+        tienne: ['Tienne', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
