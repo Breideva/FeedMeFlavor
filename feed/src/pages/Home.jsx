69db19c09f8710190b9f7951bfedafaf96feaeab
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-08-29 16:04:57 -0500

    Made home page and added header component to it

---
+++ feed/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+
+export default function Home() {
+  return (
+   <div className="h-screen w-8/12 bg-backgroundLight">
+   <NavBar />
+   <Header />
+   </div>
+  )
+}
