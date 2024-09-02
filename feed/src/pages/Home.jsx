ba965cc8c65f149399da6144382554e550ae1e4d
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-02 16:07:28 -0500

    Added sections to home page

--- feed/src/pages/Home.jsx
+++ feed/src/pages/Home.jsx
@@ -1,13 +1,16 @@
+import CategoryButtons from "../components/CategoryButtons";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Popular from "../components/Popular";
+import Regions from "../components/Regions";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full flex flex-col">
-        <NavBar />
+    <div>
         <Header />
         <Popular />
+        <Regions />
+        < CategoryButtons />
     </div>
   );
 }
