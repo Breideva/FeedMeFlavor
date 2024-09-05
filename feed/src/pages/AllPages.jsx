13bad2f8a5977e46ba720ce2d7754b9aa15ba9d7
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-05 17:14:48 -0500

    Added another route to allpages

--- feed/src/pages/AllPages.jsx
+++ feed/src/pages/AllPages.jsx
@@ -1,13 +1,14 @@
 import {Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import CategoryButtons from "../components/CategoryButtons";
 import Categories from "./Categories";
+import MainCategories from "./MainCategories";
 
 export default function AllPages() {
   return (
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/types/:categories" element={<Categories/>} />
+        <Route path="/main/:types" element={<MainCategories/>} />
       </Routes>
   )
 }
