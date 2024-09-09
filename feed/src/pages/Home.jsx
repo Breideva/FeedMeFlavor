43197e2b2df8989484321551eeb23f5b428a4f03
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-09 18:43:05 -0500

    Added new buttons

--- feed/src/pages/Home.jsx
+++ feed/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import CategoryButtons from "../components/CategoryButtons";
 import Header from "../components/Header";
+import MainButtons from "../components/MainButtons";
 import NavBar from "../components/NavBar";
 import Popular from "../components/Popular";
 import Regions from "../components/Regions";
@@ -7,10 +8,11 @@ import Regions from "../components/Regions";
 export default function Home() {
   return (
     <div>
-        <Header />
-        <Popular />
-        <Regions />
-        < CategoryButtons />
+      <Header />
+      <Popular />
+      <Regions />
+      <CategoryButtons />
+      <MainButtons />
     </div>
   );
 }
