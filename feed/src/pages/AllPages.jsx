d863452b6f25d254ae1937523129442bbc954c37
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-09 18:42:48 -0500

    Added recipe page

--- feed/src/pages/AllPages.jsx
+++ feed/src/pages/AllPages.jsx
@@ -1,14 +1,16 @@
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Categories from "./Categories";
 import MainCategories from "./MainCategories";
+import Recipes from "./Recipes";
 
 export default function AllPages() {
   return (
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/types/:categories" element={<Categories/>} />
-        <Route path="/main/:types" element={<MainCategories/>} />
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/types/:categories" element={<Categories />} />
+      <Route path="/main/:categories" element={<MainCategories />} />
+      <Route path="/items/:info" element={<Recipes />} />
+    </Routes>
+  );
 }
