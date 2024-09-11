1c4112cd04292513627c93fd34af260462be8df0
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-10 19:38:12 -0500

    Removed maincategory route

--- feed/src/pages/AllPages.jsx
+++ feed/src/pages/AllPages.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Categories from "./Categories";
-import MainCategories from "./MainCategories";
 import Recipes from "./Recipes";
 
 export default function AllPages() {
@@ -9,7 +8,7 @@ export default function AllPages() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/types/:categories" element={<Categories />} />
-      <Route path="/main/:categories" element={<MainCategories />} />
+      {/* <Route path="/main/:categories" element={<MainCategories />} /> */}
       <Route path="/items/:info" element={<Recipes />} />
     </Routes>
   );
