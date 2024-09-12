ee3e8fc2fafc7507af4660168c94e3e436098d52
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-12 15:31:26 -0500

    Updated styles for header

--- feed/src/components/Header.jsx
+++ feed/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import Food from "../assets/Food-Image.png";
 
+
 export default function Header() {
+
+
   return (
     <div className="flex items-center justify-center bg-background">
       <div className="w-9/12 flex h-screen items-center relative">
@@ -11,15 +14,16 @@ export default function Header() {
           <p className="text-xs sm:text-sm med:text-md lg:text-xl w-3/4">
             For every food and diet
           </p>
-          <button className="text-sm sm:text-md md:text-lg lg:text-2xl w-4/6 bg-primary p-4 rounded-lg shadow-lg transition-all duration-700 hover:bg-secondary">
-            Show me Burgers
+          <button to="" className="text-sm sm:text-md md:text-lg lg:text-2xl w-4/6 bg-secondary p-4 my-2 rounded-lg shadow-lg transition-all duration-500 hover:bg-primary hover:m-0 hover:py-6">
+          About This Site
           </button>
         </div>
         <img
-          className="h-1/3 sm:h-1/2 md:h-1/2 lg:h-1/2 xl:h-1/2 absolute top-0 right-0"
+          className="h-1/3 sm:h-1/2 md:h-1/2 lg:h-1/2 xl:h-1/2 absolute top-50 right-0 bg-gradient-to-b"
           src={Food}
           alt="Illustration of people eating"
         />
+     
       </div>
     </div>
   );
