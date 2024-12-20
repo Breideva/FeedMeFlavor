8c3e74c63a82500ba1bcaef73f2b169d2379fd9a
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-20 14:25:06 -0600

    Updated Navbar
    Cleaned up styling with adding function to change background once leaving the top of the page.

--- feed/src/components/NavBar.jsx
+++ feed/src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import Logo from "../assets/Logo.png";
-import { Link, useFetcher } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   MdKeyboardArrowDown,
   MdOutlineKeyboardArrowRight,
@@ -12,6 +11,11 @@ export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   const [linesOpen, setLinesOpen] = useState();
   const [moblieOpen, setMobileOpen] = useState(false);
+  const [position, setPosition] = useState(0);
+
+  const windowChange = () => {
+    setPosition(window.scrollY);
+  };
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -36,111 +40,125 @@ export default function NavBar() {
     };
   }, []);
 
+  window.addEventListener("scroll", windowChange);
+
   return (
-    <div className="bg-background flex justify-center w-full fixed top-0 left-0 z-20 text-text shadow-xl">
+    <div
+      className={`flex justify-center w-full fixed top-0 left-0 z-20 py-2 text-white transition-all duration-500
+        ${position === 0 ? "bg-transparent" : "bg-secondary"}`}
+    >
       <div className="w-9/12 flex items-center justify-between text-sm sm:text-base lg:text-lg xl:text-xl">
-        <Link to="/">
+        {/* <Link to="/">
           <img className="h-12 md:h-20 p-2" src={Logo} alt="Logo" />
-        </Link>
+        </Link> */}
         {linesOpen ? (
-          <>
-            <button onClick={mobileDropdown}>
-              <TfiMenu className="text-2xl sm:text-3xl md:text-5xl" />
-            </button>
-            {moblieOpen && (
-              <div className="bg-background absolute left-1/3 sm:right-1/2 top-full rounded-xl text-sm sm:text-base md:text-lg text-textLight">
-                <ul className="px-2 border-2 border-textLight shadow-xl rounded-xl">
-                  <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text hover:border-primary">
-                    <button
-                      className="my-2 flex items-end"
-                      onClick={toggleDropdown}
-                    >
-                      Recipes
-                      {isOpen ? (
-                        <MdKeyboardArrowDown className="w-6" />
-                      ) : (
-                        <MdOutlineKeyboardArrowRight className=" w-6" />
+          <div className="w-full flex justify-between items-center">
+            <div className="w-1/4">
+              <button onClick={mobileDropdown}>
+                <TfiMenu className="text-2xl sm:text-3xl md:text-4xl" />
+              </button>
+              {moblieOpen && (
+                <div className="bg-white absolute text-sm sm:text-base md:text-lg text-black">
+                  <ul className="flex flex-col items-start px-2 border-2 border-background">
+                    <li className="py-1 px-2 transition-all duration-500  hover:border-black border-b">
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li className="py-1 px-2 transition-all duration-500  hover:border-black border-b">
+                      <Link to="/about">About</Link>
+                    </li>
+                    <li className="py-1 px-2 transition-all duration-500 hover:border-black border-b">
+                      <button
+                        className="flex items-center"
+                        onClick={toggleDropdown}
+                      >
+                        Recipes
+                        {isOpen ? (
+                          <MdKeyboardArrowDown className="" />
+                        ) : (
+                          <MdOutlineKeyboardArrowRight className="" />
+                        )}
+                      </button>
+                      {isOpen && (
+                        <div className="bg-white absolute text-xs md:text-sm -right-1/2">
+                          <ul
+                            onClick={toggleDropdown}
+                            onMouseLeave={toggleDropdown}
+                            className="border border-textLight"
+                          >
+                            <li className="py-1 px-2 transition-all duration-500 hover:bg-background">
+                              <Link to="/types/Appetizers">Appetizers</Link>
+                            </li>
+                            <li className="py-1 px-2 transition-all duration-500  hover:bg-background">
+                              <Link to="/types/Desserts">Desserts</Link>
+                            </li>
+                            <li className="py-1 px-2 transition-all duration-500  hover:bg-background">
+                              <Link to="/types/Beverages">Beverages</Link>
+                            </li>
+                            <li className="py-1 px-2 transition-all duration-500  hover:bg-background">
+                              <Link to="/types/Snacks">Snacks</Link>
+                            </li>
+                          </ul>
+                        </div>
                       )}
-                    </button>
-                    {isOpen && (
-                      <div className="bg-background absolute text-xs md:text-base -right-1/2 text-textLight">
-                        <ul
-                          onClick={toggleDropdown}
-                          onMouseLeave={toggleDropdown}
-                          className="px-2 border-2 border-textLight shadow-xl rounded-xl"
-                        >
-                          <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text hover:border-primary">
-                            <Link to="/types/Appetizers">Appetizers</Link>
-                          </li>
-                          <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text  hover:border-primary">
-                            <Link to="/types/Desserts">Desserts</Link>
-                          </li>
-                          <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text  hover:border-primary">
-                            <Link to="/types/Beverages">Beverages</Link>
-                          </li>
-                          <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text  hover:border-primary">
-                            <Link to="/types/Snacks">Snacks</Link>
-                          </li>
-                        </ul>
-                      </div>
-                    )}
-                  </li>
-                  <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text  hover:border-primary">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text  hover:border-primary">
-                    <Link to="/about">About</Link>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
+            <div className="w-3/4">
+              <Search />
+            </div>
+          </div>
         ) : (
-          <div className="w-9/12 flex items-center justify-around">
+          <div className="w-full flex items-center justify-between">
+            <div>
+              <Link to="/" className="font-bold italic text-lg xl:text-4xl">
+                FeedMeFlavor
+              </Link>
+            </div>
+            <div className="w-1/2">
+              <Search />
+            </div>
+            <h2 className="transition-all text-sm xl:text-xl duration-500 hover:text-textLight">
+              <Link to="/about">About</Link>
+            </h2>
             <div>
               <button
-                className="my-2 flex items-end transition-all duration-500 hover:text-textLight"
+                className="flex items-center text-sm xl:text-xl transition-all duration-500 hover:text-textLight"
                 onClick={toggleDropdown}
               >
                 Recipes
                 {isOpen ? (
-                  <MdKeyboardArrowDown className="w-6" />
+                  <MdKeyboardArrowDown className="text-sm xl:text-lg" />
                 ) : (
-                  <MdOutlineKeyboardArrowRight className=" w-6" />
+                  <MdOutlineKeyboardArrowRight className="text-sm xl:text-lg" />
                 )}
               </button>
               {isOpen && (
-                <div className="bg-background absolute text-sm md:text-lg text-textLight rounded-xl">
+                <div className="bg-white absolute text-sm md:text-lg text-black mt-2 border-2 border-background">
                   <ul
                     onClick={toggleDropdown}
                     onMouseLeave={toggleDropdown}
-                    className="px-2 border-2 border-textLight shadow-xl rounded-xl"
+                    className=""
                   >
-                    <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text hover:border-primary">
+                    <li className="py-1 px-2 transition-all duration-500 hover:bg-background">
                       <Link to="/types/appetizers">Appetizers</Link>
                     </li>
-                    <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text  hover:border-primary">
+                    <li className="py-1 px-2 transition-all duration-500  hover:bg-background">
                       <Link to="/types/desserts">Desserts</Link>
                     </li>
-                    <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text  hover:border-primary">
+                    <li className="py-1 px-2 transition-all duration-500  hover:bg-background">
                       <Link to="/types/beverages">Beverages</Link>
                     </li>
-                    <li className="my-2 border-b-2 border-textLight transition-all duration-500 hover:text-text  hover:border-primary">
+                    <li className="py-1 px-2 transition-all duration-500  hover:bg-background">
                       <Link to="/types/snacks">Snacks</Link>
                     </li>
                   </ul>
                 </div>
               )}
             </div>
-            <h2 className="transition-all duration-500 hover:text-textLight">
-              <Link to="/">Home</Link>
-            </h2>
-            <h2 className="transition-all duration-500 hover:text-textLight">
-              <Link to="/about">About</Link>
-            </h2>
           </div>
         )}
-        <Search />
       </div>
     </div>
   );
