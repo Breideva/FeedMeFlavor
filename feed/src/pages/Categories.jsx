1100b775ac310caefdfcb32d8083b64845090f13
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-20 14:22:48 -0600

    Revised Categories
    Changed styles and layout of everything, as well as changed the number of calls to 12.

--- feed/src/pages/Categories.jsx
+++ feed/src/pages/Categories.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import banner from "../../public/assets/recipe-banner.png";
 
 export default function Categories() {
   const params = useParams();
   const [categories, setCategories] = useState([]);
 
   const makeCategories = async (name) => {
+    // const check = localStorage.getItem("cate");
+    // if (check) {
+    //   setCategories(JSON.parse(check));
+    // } else {
     const apiKey = import.meta.env.VITE_API_KEY;
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=8&tags=${name}`
+      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=12&tags=${name}`
     );
     const data = await api.json();
+    // localStorage.setItem("cate", JSON.stringify(data.recipes));
     setCategories(data.recipes);
+    // }
   };
 
   useEffect(() => {
@@ -19,25 +26,45 @@ export default function Categories() {
   }, [params.categories]);
 
   return (
-    <div className="bg-background flex pb-8 justify-center text-primary bg-gradient-to-b from-background to-secondary">
+    <div className="bg-secondary min-h-screen flex justify-center pb-8 text-white">
       <div className="w-9/12">
-        <h1 className="text-8xl font-bold w-fit mt-24 mb-8">
-          {params.categories.charAt(0).toUpperCase() + params.categories.slice(1)}
-        </h1>
-        <div className="grid grid-cols-1 justify-items-center gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-center">
+        <div
+          className="w-full bg-secondary flex justify-center items-center mt-20 mb-2 h-20 md:h-28 xl:h-40"
+          style={{
+            backgroundImage: `url(${banner})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <h1 className=" text-3xl sm:text-4xl md:text-5xl xl:text-6xl font-bold">
+            {params.categories.charAt(0).toUpperCase() +
+              params.categories.slice(1)}
+          </h1>
+          <div className=""></div>
+        </div>
+        <div className="grid grid-cols-1 justify-items-center bg-white sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-center">
           {categories.map((items) => (
             <Link
               to={"/items/" + items.id}
-              className="bg-backgroundLight shadow-xl rounded-lg p-4 w-full"
+              className="bg-backgroundLight w-full p-4 bg-secondary text-white hover:bg-opacity-85 transition-all duration-300"
               key={items.id}
             >
-              <h2 className="text-text text-xl h-20">{items.title}</h2>
               <img
                 loading="lazy"
                 className="w-full"
                 src={items.image}
                 alt={items.title}
               />
+              <h2 className="text-2xl font-semibold">{items.title}</h2>
+              <div className="flex w-full items-center justify-center gap-2">
+                <h3 className="text-xl sm:text-xl md:text-lg xl:text-xl font-light">
+                  Serves:
+                </h3>
+                <p className="text-xl sm:text-xl md:text-lg xl:text-xl font-medium">
+                  {items.servings}
+                </p>
+              </div>
             </Link>
           ))}
         </div>
